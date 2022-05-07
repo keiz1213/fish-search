b6ebd72d4c8728273f6e6891f8403dbf4469cdc8
keiz1213 <fukuoka00tsc@gmail.com>
2022-05-08 02:45:31 +0900

    Refactoring
    1. add Prompt class and Filter class.
    2. remove Main class.
    3. add Search::fishSearch.
    4. add Search::question.

--- index.js
+++ index.js
@@ -2,29 +2,45 @@
 
 const Fish = require('./lib/fish')
 const Choice = require('./lib/choice')
-const Search = require('./lib/search')
+const Filter = require('./lib/filter')
+const Prompt = require('./lib/prompt')
 const fishData = require('./data/fish_data')
 
-class Main {
-  static async run (search) {
-    const answer = await search.howToSearch()
-    let fishes
-    if (answer === '旬の時期から探す') {
-      fishes = await search.searchBySeason()
-    } else if (answer === 'よく釣れる時期から探す') {
-      fishes = await search.searchByFishingSeason()
-    } else if (answer === '釣り方から探す') {
-      fishes = await search.searchByHowToFish()
-    } else if (answer === '食べ方から探す') {
-      fishes = await search.searchByHowToEat()
-    } else if (answer === '魚の名前から探す') {
-      fishes = await search.searchByName()
-    }
-    search.showFishInfo(fishes)
+class Search {
+  constructor (prompt, filter) {
+    this.prompt = prompt
+    this.filter = filter
+  }
+
+  async fishSearch () {
+    const firstAnswer = await this.question()
+    const secondAnswer = await this.question(firstAnswer)
+    const fishes = this.filter.fishFilter(firstAnswer, secondAnswer)
+    this.showFishInfo(fishes)
+  }
+
+  async question (answer) {
+    const prompt = answer ? this.prompt.makePrompt(answer) : this.prompt.makePrompt()
+    return await prompt.run()
+  }
+
+  async showFishInfo (fishes) {
+    fishes.push('終了')
+    let answer
+    do {
+      const prompt = this.prompt.selectPrompt(fishes, '魚を選択してください')
+      answer = await prompt.run()
+      if (answer !== '終了') {
+        const fish = fishes.find(fish => fish.name === answer)
+        fish.fishInfo()
+      }
+    } while (answer !== '終了')
+    console.log('良い一日を！')
   }
 }
 
 const allFishes = Fish.createAllFishes(fishData)
 const choice = new Choice(allFishes)
-const search = new Search(choice)
-Main.run(search)
+const filter = new Filter(allFishes)
+const prompt = new Prompt(choice)
+new Search(prompt, filter).fishSearch()
